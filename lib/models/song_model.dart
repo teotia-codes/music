1083777b097f35b64c8d557cd4dcf4c7938a86a3
teotia-codes <priyanshu.teotia50@gmail.com>
2024-02-01 00:15:26 +0530

    added assets

---
+++ lib/models/song_model.dart
@@ -0,0 +1,28 @@
+class Song {
+  final String title;
+  final String description;
+  final String url;
+  final String coverUrl;
+Song({
+  required this.title,
+  required this.description,
+  required this.url,
+  required this.coverUrl,
+});
+
+static List<Song> songs = [
+  Song(title: 'Bachke Bachke',
+  description: 'Punjabi',
+  url: 'assets/music/aujla.mp3',
+  coverUrl: 'assets/images/1.jpeg'),
+  Song(title: 'BPee loon',
+  description: 'Romantic',
+  url: 'assets/music/kk.mp3',
+  coverUrl: 'assets/images/2.jpeg'),
+  Song(title: 'Tere Hawale',
+  description: 'Bollywood',
+  url: 'assets/music/arijit.mp3',
+  coverUrl: 'assets/images/3.jpeg'),
+  
+];
+}
--- lib/views/new.dart
+++ lib/views/new.dart
@@ -5,55 +5,115 @@ class New extends StatefulWidget {
   New({super.key});
   @override
   State<StatefulWidget> createState() {
-  return _New();
+    return _New();
   }
 }
+
 class _New extends State<New> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(gradient: LinearGradient(colors: [Color.fromRGBO(255, 85, 212, 1).withOpacity(0.7),
-      Color.fromARGB(255, 255, 233, 249).withOpacity(0.7),],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromRGBO(255, 0, 191, 1).withOpacity(0.7),
+          Color.fromARGB(255, 234, 197, 224).withOpacity(0.7),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
       ),
-      child: Scaffold(backgroundColor: Colors.transparent,
-      appBar: PreferredSize(preferredSize: Size.fromHeight(30), child: AppBar(
+      child: Scaffold(
         backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: const Icon(Icons.grid_view_rounded,color: Colors.white,),
-        actions: [
-          Icon(Icons.account_circle_outlined,color: Colors.white,
-          size: 24,
-          ),
-          SizedBox(width: 18,)
-        ],
-      ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-      unselectedItemColor: Colors.white,
-      selectedItemColor: Colors.white,
-      items: const [BottomNavigationBarItem(
-        icon:Icon(Icons.home_outlined), label: 'Home' ),
-      BottomNavigationBarItem(icon:Icon(Icons.star_border), label: 'Favourite' ),
-      BottomNavigationBarItem(icon:Icon(Icons.play_circle), label: 'Play' ),
-      BottomNavigationBarItem(icon:Icon(Icons.people_alt_outlined), label: 'Profile' ),],backgroundColor:  Color.fromARGB(255, 168, 46, 135)),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-              children:[ Text('Welcome',style: GoogleFonts.ubuntu(color: Colors.white,fontWeight: FontWeight.bold,
-              fontSize: 24),),
-              SizedBox(height: 5,),
-              Text('Enjoy your favourite music',style: GoogleFonts.ubuntu(color: Colors.white,fontWeight: FontWeight.bold,
-              fontSize: 24),),]
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(30),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: const Icon(
+              Icons.grid_view_rounded,
+              color: Colors.white,
             ),
-          )
-        ],),
-      ),),
+            actions: [
+              Icon(
+                Icons.account_circle_outlined,
+                color: Colors.white,
+                size: 30,
+              ),
+              SizedBox(
+                width: 18,
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.white,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.star_border), label: 'Favourite'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.play_circle), label: 'Play'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.people_alt_outlined), label: 'Profile'),
+            ],
+            backgroundColor: Color.fromARGB(255, 168, 46, 135)),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              _DiscoverMusic(),
+            ],
+          ),
+        ),
+      ),
     );
-  }}
+  }
+}
+
+class _DiscoverMusic extends StatelessWidget {
+  const _DiscoverMusic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(
+          'Welcome',
+          style: GoogleFonts.ubuntu(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        SizedBox(
+          height: 2,
+        ),
+        Text(
+          'Enjoy your favourite music',
+          style: GoogleFonts.ubuntu(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextFormField(
+          decoration: InputDecoration(
+            isDense: true,
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: Colors.grey.shade400),
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.grey.shade400,
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(18)),
+          ),
+        )
+      ]),
+    );
+  }
+}
